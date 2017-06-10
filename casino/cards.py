09e420a3cc1bb67d214ab2a90ef457f3ad3d5a1e
ejn88 <ericjneer@gmail.com>
2017-06-10 12:47:43 -0700

    Add BlackJack hand and use Card namedtuple for readability

--- casino/cards.py
+++ casino/cards.py
@@ -2,17 +2,25 @@
 '''
 import itertools
 import random
+from collections import namedtuple
 
-CARDS = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
-SUITS = ('H', 'D', 'C', 'S')
+Card = namedtuple('Card', ('rank', 'suit'))
+
 
 class Deck(object):
     ''' Deck(s) of cards. '''
+    CARDS = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+    SUITS = ('H', 'D', 'C', 'S')
 
     def __init__(self, num_decks=1):
-        self.deck = list(itertools.product(CARDS * num_decks, SUITS))
+        self.deck = self._create_cards(num_decks)
         self.shuffle()
         self._idx = 0
+
+    def _create_cards(self, num_decks):
+        card_suit_combos = itertools.product(
+            self.CARDS * num_decks, self.SUITS)
+        return list(Card(x[0], x[1]) for x in card_suit_combos)
 
     def __len__(self):
         return len(self.deck)
@@ -36,3 +44,32 @@ class Deck(object):
         ''' The number of cards yet to be dealt. '''
         return len(self.deck) - self._idx
 
+
+class BlackJackHand(object):
+    ''' A single hand of blackjack. '''
+
+    CARD_VALUES = dict(zip(Deck.CARDS,
+                           (*range(2, 11), 10, 10, 10, 11)  # 2-10, J, Q, K, A
+                           )
+                       )
+
+    def __init__(self, cards):
+        self.cards = cards
+
+    @property
+    def score(self):
+        ''' The numerical value of the hand. '''
+        hand_score = 0
+        for card in self.cards:
+            if 'A' in card:
+                card_score = self._handle_ace(hand_score)
+            else:
+                card_score = self.CARD_VALUES[card.rank]
+            hand_score += card_score
+        return hand_score
+
+    def _handle_ace(self, current_hand_score):
+        if current_hand_score > 10:
+            return 1
+        else:
+            return 11
--- tests/test_cards.py
+++ tests/test_cards.py
@@ -2,10 +2,11 @@ import unittest
 
 from ..casino import cards
 
+
 class TestDeck(unittest.TestCase):
     def setUp(self):
         self.deck = cards.Deck()
-    
+
     def test_deck_multiplier(self):
         deck = cards.Deck(2)
         self.assertEqual(len(deck), 104)
@@ -16,13 +17,32 @@ class TestDeck(unittest.TestCase):
         self.assertListEqual(first_five_cards_copy, first_five_dealt)
 
     def test_cards_remaining(self):
-        # simulate 50 cards previously dealt
-        self.deck._idx = 50
+        self.deck.deal(50)
         self.assertEqual(self.deck.cards_remaining, 2)
 
     def test_not_enough_cards_remaining(self):
         with self.assertRaises(ValueError):
             self.deck.deal(53)
+
+
+class TestBlackJackHand(unittest.TestCase):
+
+    def test_hand_score_no_aces(self):
+        hand = cards.BlackJackHand([cards.Card('Q', ''),
+                                    cards.Card('K', '')])
+        actual = hand.score
+        expected = 20
+        self.assertEqual(expected, actual)
+
+    def test_hand_score_with_aces(self):
+        hand = cards.BlackJackHand([cards.Card('A', ''),
+                                    cards.Card('A', ''),
+                                    cards.Card('7', ''),
+                                    cards.Card('2', '')])
+        actual = hand.score
+        expected = 21
+        self.assertEqual(expected, actual)
+
 
 if __name__ == '__main__':
     unittest.main()
